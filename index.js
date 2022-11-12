b38cff618ed75d67d94463d07837087052928bac
vipul71098 <38537174+vipul71098@users.noreply.github.com>
2022-11-12 13:04:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var server = http.createServer(function(request, response) {
 
 });
 
-var port = 80;
+var port = process.env.PORT || 5000;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
